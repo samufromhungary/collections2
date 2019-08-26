8a7ea96260963582afc8bc30d4cf845eeee1f37f
samufromhungary <blzkproblem@gmail.com>
2019-08-26 09:48:54 +0200

    Add project files.

---
+++ DictionaryCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("0", "zero");
+            hashtable.Add("1", "one");
+            hashtable.Add("2", "two");
+            hashtable.Add("3", "three");
+            hashtable.Add("4", "four");
+            hashtable.Add("5", "five");
+            hashtable.Add("6", "six");
+            hashtable.Add("7", "seven");
+            hashtable.Add("8", "eight");
+            hashtable.Add("9", "nine");
+
+
+            string variable = "072542795409752195734ö96078436219409";
+
+            foreach(char c in variable){
+                string newVariable = c.ToString();
+                if (hashtable.ContainsKey(newVariable)){
+                    Console.WriteLine(hashtable[newVariable]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
